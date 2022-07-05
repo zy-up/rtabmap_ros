f538b8c3c70076979743da110dd246a903d05ba1
zy-up <zhengyu1997114@gmail.com>
2022-07-05 23:15:09 +0900

    update .gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/exdx/codes/catkin_rtab/src/rtabmap_ros
-# Build directory: /home/exdx/codes/catkin_rtab/src/rtabmap_ros/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
