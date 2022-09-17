9203ce51d59b0e7acdff732baa0d02485bd45b3d
zy-up <zhengyu1997114@gmail.com>
2022-09-17 17:56:05 +0900

    renamed:    launch/orchard/Camera setup/H_kinect.launch -> launch/orchard/Camera_setup/H_kinect.launch 	renamed:    launch/orchard/Camera setup/L_kinect.launch -> launch/orchard/Camera_setup/L_kinect.launch 	renamed:    launch/orchard/Camera setup/M_kinect.launch -> launch/orchard/Camera_setup/M_kinect.launch 	renamed:    launch/orchard/mapping/statsic_tf.launch -> launch/orchard/Camera_setup/statsic_tf.launch 	renamed:    launch/orchard/mapping/statsic_tf_two_camera.launch -> launch/orchard/Camera_setup/statsic_tf_two_camera.launch 	new file:   launch/orchard/Camera_setup/three_cameras.sh

---
+++ launch/orchard/Camera_setup/three_cameras.sh
@@ -0,0 +1,14 @@
+echo -e "\033[41;32mrun statsic_tf.launch\033[0m"
+roslaunch rtabmap_ros statsic_tf.launch & sleep 3
+
+echo -e "\033[41;32mrun L_kinect.launch\033[0m"
+roslaunch rtabmap_ros L_kinect.launch & sleep 3
+
+echo -e "\033[41;32mrun M_kinect.launch\033[0m"
+roslaunch rtabmap_ros M_kinect.launch & sleep 3
+
+echo -e "\033[41;32mrun H_kinect.launch\033[0m"
+roslaunch rtabmap_ros H_kinect.launch & sleep 3
+
+wait
+exit 0
