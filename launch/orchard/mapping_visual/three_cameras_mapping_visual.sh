0ca9b8558f668f4a53e38314518268755877292e
zy-up <zhengyu1997114@gmail.com>
2022-12-22 16:58:21 +0900

    重命名：   launch/orchard/mapping_pointcloud/three_cameras_mapping.sh -> launch/orchard/mapping_pointcloud/three_cameras_mapping_pointcloud.sh 	重命名：   launch/orchard/mapping_visual/three_cameras_mapping.sh -> launch/orchard/mapping_visual/three_cameras_mapping_visual.sh 	修改：     launch/orchard/record_play/three_cameras_record.sh 	新文件：   sh/three_cameras_mapping_pointcloud.sh 	新文件：   sh/three_cameras_mapping_visual.sh

---
+++ launch/orchard/mapping_visual/three_cameras_mapping_visual.sh
@@ -0,0 +1,25 @@
+echo -e "\033[41;32mrun statsic_tf.launch\033[0m"
+roslaunch rtabmap_ros statsic_tf.launch & sleep 3
+
+echo -e "\033[41;32mrun H_kinect.launch\033[0m"
+roslaunch rtabmap_ros H_kinect_Syn.launch & sleep 3
+
+echo -e "\033[41;32mrun M_kinect.launch\033[0m"
+roslaunch rtabmap_ros M_kinect_Syn.launch & sleep 3
+
+echo -e "\033[41;32mrun L_kinect.launch\033[0m"
+roslaunch rtabmap_ros L_kinect_Syn.launch & sleep 3
+
+echo -e "\033[41;32mrun L_apriltag_detect.launch\033[0m"
+roslaunch rtabmap_ros L_apriltag_detect.launch & sleep 1
+
+# current date
+timestamp=$(date +%Y_%m_%d_%H_%M_%S)
+database_path="/home/sbdx/Documents/"$timestamp".db"
+echo $database_path
+
+echo -e "\033[41;32mrun three_kinects_in_Rtabmap.launch\033[0m"
+gnome-terminal --tab --active -e "roslaunch rtabmap_ros three_kinects_in_Rtabmap.launch database_path:=$database_path"
+
+wait
+exit 0
